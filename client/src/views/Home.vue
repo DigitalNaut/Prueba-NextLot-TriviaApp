3ee3d57a5a9efc41d84b098282e5ff997e787fb0
Cybernaut <77c828fa@opayq.com>
2021-03-07 20:01:14 -0600

    First commit
    + Basic setup

---
+++ client/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script lang='ts'>
+
+import { Vue } from 'vue-class-component'
+
+export default class Home extends Vue {
+  message='Hello World'
+}
+</script>
