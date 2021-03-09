70525fb7da7306dc6a50b849c7b59e1340d641c7
Cybernaut <77c828fa@opayq.com>
2021-03-09 01:49:10 -0600

    Update facts.ts
    + Changed regex to string for readability

--- routes/facts.ts
+++ routes/facts.ts
@@ -5,12 +5,11 @@ import { getNewFact } from "../public/javascripts/controller";
 const router = express.Router();
 
 /* GET new Session ID. */
-router.get(/\/facts\/new/,
+router.get('/facts\/new',
   (_, res) => {
     res.send("Hi");
   });
 
 router.get('/facts', (_, res) => { res.redirect('/facts\/new'); });
-
 
 export default router;
